e4b96b65209aa0cfedca99fd36c2aa45b3fe7aeb
h1_0ne <den4ik-tv@yandex.ru>
2019-01-28 20:18:05 +0300

    None

---
+++ VkPlayer/authorization_form.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace VkPlayer
+{
+    public partial class authorization_form : Form
+    {
+        public authorization_form()
+        {
+            InitializeComponent();
+            if (this.Owner is Main main)
+            {
+                main.LoadText();
+                login.Font = main.Roboto_thin;
+                password.Font = main.Roboto_thin;
+                label1.Font= main.Roboto_thin;
+                label2.Font = main.Roboto_thin;
+            }
+            this.FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Width = 327;
+            Height = 143;
+            twoFact_check.Location = new Point(7, 74);
+            auth_btn.Location = new Point(176, 66);
+            auth_btn.FlatAppearance.BorderSize = 0;
+            auth_btn.FlatStyle = FlatStyle.Flat;
+        }
+
+        private void auth_btn_Click(object sender, EventArgs e)
+        {
+            if (this.Owner is Main main)
+            {
+                
+                if (!twoFact_check.Checked)
+                {
+                    try
+                    {
+                        main.GetAuth(false, login.Text, password.Text);
+                        Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        File.WriteAllText("someFile.tempdat", "");
+                        main.GetAuth(true, login.Text, password.Text);
+                        Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Неверный логин или пароль", "Ошибка", MessageBoxButtons.OK);
+                    }
+                }             
+            }
+        }
+    }
+}
