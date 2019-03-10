507c67ae943596807adc5e0954824fb854ecddd8
h10ne <44882885+h10ne@users.noreply.github.com>
2019-03-10 03:06:08 +0300

    README

--- VkPlayer/Properties/AssemblyInfo.cs
+++ VkPlayer/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 //[assembly: AssemblyVersion("1.1.*")]
-[assembly: AssemblyVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.4.0")]
 //[assembly: AssemblyFileVersion("1.1.0.0")]
